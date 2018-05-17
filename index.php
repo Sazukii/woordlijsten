f47f83288a77c162ac5866cc459670a0787033c5
Paco Lee <pacootjelee3110@gmail.com>
2018-05-17 10:44:19 +0200

    index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-
-echo "test";
+?>
+</<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Woord Lijsten</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    
+</body>
+</html>
