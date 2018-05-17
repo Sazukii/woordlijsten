37fd9ce702238980bfbeaa425d1e1c2c7598649d
unknown <tygovanommen@live.nl>
2018-05-17 10:49:04 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "test";
+$data = file_get_contents('OpenTaal-210G-basis-ongekeurd.txt');
+
+$words  = explode(' ', $data);
+
+$longestWordLength = 0;
+$longestWord = '';
+
+foreach ($words as $word) {
+    if (strlen($word) > $longestWordLength) {
+        $longestWordLength = strlen($word);
+        $longestWord = $word;
+    }
+}
+
+echo $longestWord;
+
+
